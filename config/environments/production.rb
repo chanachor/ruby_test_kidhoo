057321a468dfde06cc8fc9e60a489ad4c59e408a
vagrant <vagrant@rails-dev-box>
2014-10-08 15:09:41 +0000

    added yui css compression to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,11 @@ Kidhoo::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
+
+
+
   # Compress JavaScripts and CSS.
+  config.assets.css_compressor = :yui
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
